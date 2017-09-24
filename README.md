5aefbec4bb236d76b235b85d7268752e0520fcf1
hxl <2240932126@qq.com>
2017-09-25 00:10:47 +0800

    da

--- README.md
+++
@@ -1,1 +0,0 @@
-# Unit8_Demo
--- app/src/main/java/com/xingkong/unit8_demo/MainActivity.java
+++ app/src/main/java/com/xingkong/unit8_demo/MainActivity.java
@@ -1,13 +1,45 @@
 package com.xingkong.unit8_demo;
 
-import android.support.v7.app.AppCompatActivity;
+import android.app.Notification;
+import android.app.NotificationManager;
+import android.app.PendingIntent;
+import android.content.Context;
+import android.content.Intent;
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
 import android.os.Bundle;
+import android.support.v4.app.NotificationCompat;
+import android.support.v7.app.AppCompatActivity;
+import android.view.View;
 
 public class MainActivity extends AppCompatActivity {
-
+    private NotificationManager notificationManager;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        notificationManager = (NotificationManager)getSystemService(Context.NOTIFICATION_SERVICE);
+    }
+
+    public void simpleNoty(View view){
+        Bitmap btm = BitmapFactory.decodeResource(getResources(),
+                               R.mipmap.ic_smail);
+        NotificationCompat.Builder builder = new NotificationCompat.Builder(this);
+        Intent intent = new Intent(this, MainActivity.class);  //需要跳转指定的页面
+        PendingIntent pendingIntent = PendingIntent.getActivity(this, 0, intent, PendingIntent.FLAG_UPDATE_CURRENT);
+        builder.setContentIntent(pendingIntent);
+        builder.setSmallIcon(R.mipmap.ic_smail);// 设置图标
+        builder.setContentTitle("外卖订单的通知");// 设置通知的标题
+        builder.setContentText("内容");// 设置通知的内容
+        builder.setWhen(System.currentTimeMillis());// 设置通知来到的时间
+     //   builder.setAutoCancel(true); //自己维护通知的消失
+        builder.setTicker("new message");// 第一次提示消失的时候显示在通知栏上的
+        builder.setOngoing(true);
+        builder.setLargeIcon(btm);
+        builder.setNumber(20);
+
+        Notification notification = builder.build();
+        notification.flags = Notification.FLAG_NO_CLEAR;  //只有全部清除时，Notification才会清除
+        notificationManager.notify(0,notification);
     }
 }
